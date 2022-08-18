d22a5140e1a2368668e5ee86fadebaadfef38ba4
Michael Wachell <michael@noiz.gg>
2022-08-18 14:41:49 -0700

    center and good

--- src/components/BaseContainer.vue
+++ src/components/BaseContainer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-64">
-<MikeIntro />
+  <div class="force p-10 flex-col w-screen h-screen items-center justify-center bg-blue-300">
+    <MikeIntro />
   </div>
 </template>
 
@@ -14,24 +14,18 @@ export default {
   },
   components: {
     MikeIntro
-}
-}
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.force {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  background-color: azure;
 }
 </style>
--- src/components/MikeIntro.vue
+++ src/components/MikeIntro.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="w-64">
-    <img src="../assets/Mike.jpg" />
-    <h2>Hi, My name is Mike</h2>
-    <blockquote>
-      I am a <span class="text-teal-200">Leader</span> a <span>software engineer</span> and a <span>creator</span>
-    </blockquote>
+  <div class="hero-container">
+    <div class="">
+      <div class="me-image-container float-around rounded-full">
+        <!-- <img src="../assets/Mike.jpg" class="me-image" /> -->
+      </div>
+      <div class="">
+        <h2 class="text-slate-500">Hi, My name is Mike</h2>
+        <blockquote class="text-slate-600">
+          I am a <span class="text-teal-500 animate-pulse">Leader</span> a <span class="text-teal-500">software
+            engineer</span> and a
+          <span class="text-teal-500">creator</span>
+        </blockquote>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -18,22 +27,33 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style scoped lang="scss">
+.hero-container {
+  @include row-center;
+  width: 50%;
+
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.me-image-container {
+  // write a circular clipping mask for this image
+  width: 300px;
+  height: 300px;
+  // add thick drop shadow to the image
+         box-shadow: 100px 100px 300px rgba(0, 0, 0, 1);
+        border: 4px solid black;
+
+  // clip the image to a circle
+  
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: url(../assets/Mike.jpg);
+
+
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+.me-image {
 
-a {
-  color: #42b983;
+
 }
 </style>
